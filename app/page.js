4b29cacd3f4222af2f20a705e7a1a4a7942a8720
holaliamewu <me.amewu@gmail.com>
2025-02-04 00:33:50 +0000

    bento almost done

--- app/page.js
+++ app/page.js
@@ -1,39 +1,80 @@
+"use client";
+
+import { useState } from "react";
+import * as motion from "motion/react-client"
 import Image from "next/image";
 import { InstagramIcon, Linkedin01Icon, Behance02Icon } from "hugeicons-react";
+import profile  from "./../public/profile.jpg";
 
 export default function Home() {
+
+  const [ status, setStatus ] = useState("absolute")
+  const cardVariants = {
+    hovered: {
+      
+    }
+  }
   return (
-      <div className=" bg-white w-screen h-screen " >
-        <main className="flex  items-center justify-center relative w-1/2 aspect-square mx-auto  " >
-        <span 
-        className="flex flex-col items-center justify-center w-[226px] aspect-square bg-red-100 
-        shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[26deg] " 
-        >box 6</span>
-          <span 
-          className="flex flex-col items-center justify-center w-[226px] aspect-square bg-yellow-100 
-          shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[22deg]  " 
-          >box 5</span>
-          <span 
-          className="flex flex-col items-center justify-center w-[226px] aspect-square bg-teal-100 
-          shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[18deg]  " 
-          >box 3</span>
-          <span 
-          className="flex flex-col items-center justify-center w-[226px] aspect-square bg-blue-100 
-          shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[14deg] " 
-          >box 4</span>
-          <span 
-          className="flex flex-col items-center justify-center w-[226px] aspect-square bg-orange-100 
-          shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[10deg] " 
-          >box 2</span>
-          <span 
-          className="flex flex-col items-center justify-center w-[226px] aspect-square 
-          shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[6deg] bg-linear-to-bl from-violet-500 to-fuchsia-500  " 
+      <div className="flex flex-col items-center justify-center bg-white w-screen h-screen " >
+        <div className="w-full aspect-square relative " >
+        <motion.main 
+        className="grid grid-cols-3 relative w-[600px] h-[300px] mx-auto "
+        >
+        <motion.div className="flex flex-col items-center justify-center w-1/6 h-1/3 bg-red-100 
+        shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[26deg] absolute " 
+        initial={{rotate: 26}}          
+        whileHover={{y: -16, rotate: 0}}
+        animate={{position: status, rotate: status === "static" ? 0 : 26,}}
+        layout
+        >box 6</motion.div>
+          <motion.div 
+          className="flex flex-col items-center justify-center w-4/6 h-2/3 bg-yellow-100 
+          shadow-lg rounded-[24px]  top-1/3 left-1/3  " 
+          initial={{rotate: 22}}          
+          whileHover={{y: -16, rotate: 0}}
+          animate={{position: status, rotate: status === "static" ? 0 : 22 }}
+          layout
+          >box 5</motion.div>
+          <motion.div 
+          className="flex flex-col items-center justify-center w-2/6 h-1/3 bg-blue-100 
+          shadow-lg rounded-[24px]  top-1/3 left-1/3  " 
+          initial={{rotate: 18}}          
+          whileHover={{y: -16, rotate: 0}}
+          animate={{position: status, rotate: status === "static" ? 0 : 18,}}
+          layout
+          >box 4</motion.div>
+          <motion.div 
+          className="flex flex-col items-center justify-center w-1/3 h-2/3 bg-teal-100 
+          shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[18deg]  absolute" 
+          initial={{rotate: 14}}          
+          whileHover={{y: -16, rotate: 0}}
+          animate={{position: status, rotate: status === "static" ? 0 : 14,}}
+          layout
+          >box 3</motion.div>
+          <motion.div 
+          className="flex flex-col items-center justify-center w-4/6 bg-orange-100 
+          shadow-lg rounded-[24px]  top-1/3 left-1/3 rotate-[10deg] absolute" 
+          initial={{rotate:10}}
+          whileHover={{y: -16, rotate: 0}}
+          animate={{position: status, rotate: status === "static" ? 0 : 10,}}
+          layout
           >
-            <p className="absolute top-2 left-4 text-[20px] font-bold" >Benjamin<br/> Kankam</p>
-            <p className="absolute bottom-2 right-4 text-[14px] font-light " >Graphic<br/> Designer</p>
-          </span>
+            <Image src={profile} className="rounded-[24px] w-[226px] aspect-square" />
+          </motion.div>
+          <motion.div 
+          className="flex flex-col items-center justify-between w-1/6 h-2/3
+          shadow-lg rounded-[24px]  top-1/3 left-1/3 p-2 bg-white absolute cursor-pointer " 
+          onClick={()=> setStatus("static")}
+          initial={{rotate: 6}}          
+          whileHover={{y: -16, rotate: 0}}
+          animate={{position: status, rotate: status === "static" ? 0 : 6,}}
+          >
+            <p className="text-[20px] font-bold mr-auto" >Benjamin<br/> Kankam</p>
+            <p className="text-[14px] font-light ml-auto  " >Graphic<br/> Designer</p>
+          </motion.div>
           <div className="h-14 bg-linear-65 from-purple-500 to-pink-500"></div>
-        </main>
+        </motion.main>
+        </div>
         <footer className="flex flex-col items-center absolute bottom-12 w-full text-[14px] text-[#848884]  " >
           <p className="" >benjaminkankamboadu@gmail.com</p>
           <p className="" >+233 249 027 640</p>
